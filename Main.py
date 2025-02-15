41c4ece170f8f046f96a7b8f8ab45ab223d5dca5
Silver Team <145873582+waxGames@users.noreply.github.com>
2025-02-15 11:14:18 +0300

    Update Langs and Mini Update Main.py

--- Main.py
+++ Main.py
@@ -2,59 +2,68 @@ import threading
 import pyautogui
 import keyboard
 import time
+import json
 import os
 
-pickaxe = ""
+pick_time = ""
+lang = ""
 
-print("Bu program @SilverTeam tarafindan geliştirilmiştir, iyi kullanimlar.\nÇikmak için 'O'-'o' tuşuna basa bilirsiniz.")
+def lang_select():
+    global lang
+    lang_selec = input("Pls select langs[tr, en, ru]: ")
+    lang_ = lang_selec.lower()
+    if lang_ == "ru":
+        lang = "ru"
+    elif lang_ == "en":
+        lang = "en"
+    elif lang_ == "tr":
+        lang = "tr"
+    else:
+        print("Please select valid lang.")
+        return lang_select()
+    
+lang_select()
+
+with open(f"langs/{lang}.json", "r", encoding="utf-8") as file:
+    lang_data = json.load(file)
+
+print(lang_data["creator_message"])
 screen_width, screen_height = pyautogui.size()
 center_x = screen_width // 2
 center_y = screen_height // 2
 
 def select_pickaxe():
-    global pickaxe
-    pickaxes = ["wooden", "tahta", "tas", "stone", "rock", "iron", "demir", "altin", "gold", "golden", "diamond", "elmas", "netherite"]
-    pickaxe_input = input("Lütfen Kazma seç: ")
-    if pickaxe_input.lower() in pickaxes:
-        print(f"{pickaxe_input.lower()} kazma başarıyla seçildi!")
-        pickaxe = str(pickaxe_input.lower())
-    else:
-        print("Geçerli bir kazma seçin, 'tahta, tas, demir, altin, elmas, netherite'.")
-        pickaxe = ""
-        return select_pickaxe()
+    global pick_time
+    with open("pickaxes.json", "r", encoding="utf-8") as file:
+        pick_data = json.load(file)
+
+    pickaxes = pick_data[lang]
+
+    while True:
+        pickaxe_input = input("Lütfen Kazma seç: ").lower()
+        for pick in pickaxes:
+            if pickaxe_input in pick["type"]:
+                pick_time = pick["time"]
+                return
+
+        print(lang_data["error_1"])
+
 
 select_pickaxe()
 
 def loop():
     while True:
-        global pickaxe
-            
-        if pickaxe == "wooden".lower() or pickaxe == "tahta".lower():
-            seconds = 4.6
-        elif pickaxe == "stone".lower() or pickaxe == "rock".lower() or pickaxe == "tas".lower():
-            seconds = 2.26
-        elif pickaxe == "iron".lower() or pickaxe == "demir".lower():
-            seconds = 1.6
-        elif pickaxe == "gold".lower() or pickaxe == "golden".lower() or pickaxe == "altin".lower():
-            seconds = 0.76
-        elif pickaxe == "diamond".lower() or pickaxe == "elmas".lower():
-            seconds = 1.3
-        elif pickaxe == "netherite".lower():
-            seconds = 1.06
-        else:
-            print("Error!")
-            os._exit(0)
-
+        global pick_time
         pyautogui.moveTo(x=center_x, y=center_y)
         pyautogui.mouseDown()
         pyautogui.press("a")
-        time.sleep(seconds)
+        time.sleep(pick_time)
         pyautogui.press("d")
 
 def q_pressed():
     while True:
         if keyboard.is_pressed("o"):
-            print("Program kapatılıyor...")
+            print(lang_data["program"])
             os._exit(0) 
 
 thread_q = threading.Thread(target=q_pressed, daemon=True)
